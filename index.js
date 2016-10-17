76371ff0f4dfc7b186b12e9e52463999f2d32e95
Andreas Gasser <andreas.safe@gmail.com>
2016-10-17 21:55:38 +0200

    #4 trying something

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || 5000;
 
 app.use('/', express.static("public"));
 
-app.listen(port, function() { console.log(`listening on port ${port}`)});
+app.listen(5000, function() { console.log('listening on port 5000')});
