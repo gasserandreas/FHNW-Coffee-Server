dee2ec453c36af35f6d0d883b45d8c6cbd2546d9
Andreas Gasser <andreas.safe@gmail.com>
2016-10-17 21:50:06 +0200

    #4 add next dokku config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 80;
 
 app.use('/', express.static("public"));
 
-app.listen(5000, function() { console.log('listening on port 5000')});
+app.listen(port, function() { console.log(`listening on port ${port}`)});
