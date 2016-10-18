8eb1b239280a7647f2ed45244ab89c83d1cf8c3e
Andreas Gasser <andreas.safe@gmail.com>
2016-10-18 08:06:34 +0200

    #4 added port.listen methode

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const mongoose = require('mongoose');
 
 //const jwtConfig = require('./config/jwt.config');
 //const mongoConfig = require('./config/mongo.config');
-const User = require('./app/models/user');
+//const User = require('./app/models/user');
 
 var app = express();
 
+// basic configuration
 const port = process.env.PORT || 5000;        // set our port
 const env = process.env.ENV || 'prod'; 
 
-// ROUTES FOR OUR API
-// =============================================================================
-const router = express.Router();              // get an instance of the express Router
+// config api router
+const router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -31,4 +31,6 @@ app.use('/api/v1', router);
 
 if (env !== 'prod') {
   app.listen(port, function() { console.log('listening on port: ' + port)});
+} else {
+  app.listen(port);
 }
