51ab4376c10a4b922fb14e973005daec63b72aff
Andreas Gasser <andreas.safe@gmail.com>
2016-10-18 14:24:24 +0200

    #11 removed unneded files

--- config/jwt.config.js
+++
@@ -1,6 +0,0 @@
-const jwtConfig = {
-  secret: new Buffer('p3FtPI_NouoeuEYt2J4LbmPrIWXSVI4wQfNtwReGoAqoHIEuAs7yVpNJyae0qb8q', 'base64'),
-  audience: 'XMEqaBGYJdOUREl9IDZoOYOXbIUbqmY7',
-};
-
-module.exports = jwtConfig;
